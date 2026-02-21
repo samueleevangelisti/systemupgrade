06bda75298b16304ac447f7a435c4b85a8a21473
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2026-02-21 11:06:51 +0100

    .

--- utils/logs.py
+++ utils/logs.py
@@ -8,16 +8,15 @@ from logging import Formatter
 from logging.handlers import TimedRotatingFileHandler
 import os
 import inspect
+import traceback
 
 from utils.configs.logs import configs
 from utils import colors
 from utils import paths
 from utils import datetimes
-from utils import converts
 
 
 
-# TODO DSE bisogna creare le funzioni anche per questi nuovi livelli di log
 _QUERY = logging.DEBUG + 1
 _QUERY_SUCCESS = logging.DEBUG + 2
 _QUERY_ERROR = logging.DEBUG + 3
@@ -25,6 +24,7 @@ _REQUEST = logging.DEBUG + 4
 _REQUEST_SUCCESS = logging.DEBUG + 5
 _REQUEST_ERROR = logging.DEBUG + 6
 _SUCCESS = logging.INFO + 1
+_WARNING_EXCEPTION = logging.WARNING + 1
 
 
 
@@ -47,45 +47,6 @@ class LogException(Exception):
 
 
 
-class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
-    '''
-    Timed rotating file handler with correct renaming
-    '''
-
-
-
-    # pylint: disable-next=unused-argument
-    def namer(self, file_name):
-        '''
-        Overrides
-        ---------
-        TimedRotatingFileHandler.namer
-        '''
-        return paths.resolve_path(paths.get_folder_path(self.baseFilename), f"{datetimes.now().isoformat()}-{paths.get_file_name(self.baseFilename)}")
-
-
-
-    def rotator(self, source, dest):
-        '''
-        Overrides
-        ---------
-        TimedRotatingFileHandler.rotator
-        '''
-        if paths.is_entry(source):
-            os.rename(source, dest)
-            with open(dest, 'rb') as source_file:
-                with open(f"{dest}.zstd", 'wb') as destination_file:
-                    destination_file.write(converts.bytes_to_zstd(source_file.read()))
-            os.remove(dest)
-
-
-
-
-
-
-
-
-
 class CustomFormatter(Formatter):
     '''
     Formatter for utc time and colored log
@@ -93,12 +54,15 @@ class CustomFormatter(Formatter):
     levelno_color_map = {
         logging.DEBUG: colors.PURPLE,
         _QUERY: colors.ORANGE,
+        _QUERY_SUCCESS: colors.GREEN,
+        _QUERY_ERROR: colors.RED,
         _REQUEST: colors.BLUE,
         _REQUEST_SUCCESS: colors.GREEN,
         _REQUEST_ERROR: colors.RED,
         logging.INFO: colors.NONE,
         _SUCCESS: colors.GREEN,
         logging.WARNING: colors.YELLOW,
+        _WARNING_EXCEPTION: colors.YELLOW,
         logging.ERROR: colors.RED,
         logging.CRITICAL: colors.RED
     }
@@ -111,7 +75,7 @@ class CustomFormatter(Formatter):
         ---------
         Formatter.__init__
         '''
-        Formatter.__init__(self, fmt=f"{colors.GREY}[%(asctime)s] %(process)d:%(thread)d %(module)s:%(funcName)s{colors.NONE} %(log_color)s(%(levelname)s) %(message)s{colors.NONE}")
+        Formatter.__init__(self, fmt=f'{colors.GREY}[%(asctime)s] %(process)d:%(thread)d %(module)s:%(funcName)s{colors.NONE} %(log_color)s(%(levelname)s) %(message)s{colors.NONE}')
 
 
 
@@ -131,7 +95,7 @@ class CustomFormatter(Formatter):
         ---------
         Formatter.formatMessage
         '''
-        record.message = record.message.replace(colors.NONE, f"{colors.NONE}{CustomFormatter.levelno_color_map[record.levelno]}")
+        record.message = record.message.replace(colors.NONE, f'{colors.NONE}{CustomFormatter.levelno_color_map[record.levelno]}')
         record.log_color = CustomFormatter.levelno_color_map[record.levelno]
         return Formatter.formatMessage(self, record)
 
@@ -159,10 +123,13 @@ def custom_log_record_factory(*args, **kwargs):
 
 logging.setLogRecordFactory(custom_log_record_factory)
 logging.addLevelName(_QUERY, 'QUERY')
+logging.addLevelName(_QUERY_SUCCESS, 'QUERY SUCCESS')
+logging.addLevelName(_QUERY_ERROR, 'QUERY ERROR')
 logging.addLevelName(_REQUEST, 'REQUEST')
 logging.addLevelName(_REQUEST_SUCCESS, 'REQUEST SUCCESS')
 logging.addLevelName(_REQUEST_ERROR, 'REQUEST ERROR')
 logging.addLevelName(_SUCCESS, 'SUCCESS')
+logging.addLevelName(_WARNING_EXCEPTION, 'WARNING EXCEPTION')
 
 
 
@@ -173,11 +140,12 @@ console_handler = StreamHandler()
 console_handler.setFormatter(custom_formatter)
 _LOGGER.addHandler(console_handler)
 if configs.IS_FILE:
-    if not paths.is_entry(configs.FOLDER_PATH):
-        os.makedirs(configs.FOLDER_PATH)
-    if not paths.is_folder(configs.FOLDER_PATH):
-        raise LogException(f"`{configs.FOLDER_PATH}` is not a folder")
-    file_handler = CustomTimedRotatingFileHandler(configs.LOG_PATH, when='midnight')
+    folder_path = paths.get_folder_path(configs.LOG_PATH)
+    if not paths.is_entry(folder_path):
+        os.makedirs(folder_path)
+    if not paths.is_folder(folder_path):
+        raise LogException(f'`{folder_path}` is not a folder')
+    file_handler = TimedRotatingFileHandler(configs.LOG_PATH, when='midnight', backupCount=configs.RETENTION_DAYS, encoding='utf-8', delay=True, utc=True)
     file_handler.setFormatter(custom_formatter)
     _LOGGER.addHandler(file_handler)
 
@@ -206,6 +174,32 @@ def query(text):
         Text of the log
     '''
     logging.log(_QUERY, text)
+
+
+
+def query_success(text):
+    '''
+    Print a query log
+    
+    Parameters
+    ----------
+    text : str
+        Text of the log
+    '''
+    logging.log(_QUERY_SUCCESS, text)
+
+
+
+def query_error(text):
+    '''
+    Print a query log
+    
+    Parameters
+    ----------
+    text : str
+        Text of the log
+    '''
+    logging.log(_QUERY_ERROR, text)
 
 
 
@@ -284,6 +278,20 @@ def warning(text):
         Text of the log
     '''
     logging.warning(text)
+
+
+
+def warning_exception(text):
+    '''
+    Print a warning log
+    
+    Parameters
+    ----------
+    text : str
+        Text of the log
+    '''
+    actual_text = f'{text}\n{traceback.format_exc()}'
+    logging.log(_WARNING_EXCEPTION, actual_text)
 
 
 
