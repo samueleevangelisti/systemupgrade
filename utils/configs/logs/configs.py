9528ac2c7dbdb361821b2fb5ab9c37a26b855ddd
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2026-02-21 11:12:51 +0100

    .

--- utils/configs/logs/configs.py
+++ utils/configs/logs/configs.py
@@ -7,5 +7,5 @@ from utils import paths
 
 IS_DEBUG = False
 IS_FILE = False
-FOLDER_PATH = paths.resolve_path(paths.get_folder_path(__file__), '../../../logs/')
-LOG_PATH = paths.resolve_path(FOLDER_PATH, 'log.log')
+LOG_PATH = paths.resolve_path(paths.get_folder_path(__file__), '../../../logs/log.log')
+RETENTION_DAYS = 365
